37560dcf32eb297a81a3cb59e8e888768e0a15ad
Henry Lin <henry.ys.lin@gmail.com>
2022-10-09 10:54:14 +0800

    Custom evaluator for rust

--- Rust/make_ten/src/main.rs
+++ Rust/make_ten/src/main.rs
@@ -1,12 +1,87 @@
-use fasteval::{ez_eval, EmptyNamespace};
+const DIGITS: &'static [char] = &['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const SYMBOLS: &'static [char] = &['+', '-', '*', '/'];
 
-const DIGITS: &'static [&'static str] = &["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-const SYMBOLS: &'static [&'static str] = &["+", "-", "*", "/"];
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum Operator {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+    BracketAdd,
+    BracketSubtract,
+    BracketMultiply,
+    BracketDivide,
+}
+
+fn get_op(ch: char, in_bracket: bool) -> Operator {
+    match ch {
+        '+' => if in_bracket { Operator::BracketAdd } else { Operator::Add },
+        '-' => if in_bracket { Operator::BracketSubtract } else { Operator::Subtract },
+        '*' => if in_bracket { Operator::BracketMultiply } else { Operator::Multiply },
+        '/' => if in_bracket { Operator::BracketDivide } else { Operator::Divide },
+        _ => panic!("Unknown operator: {}", ch)
+    }
+}
+
+fn eval_op(operands: &mut Vec<f64>, operators: &mut Vec<Operator>, idx: usize) {
+    let operator = operators[idx];
+    let operand1 = operands[idx];
+    let operand2 = operands[idx + 1];
+
+    match operator {
+        Operator::Add | Operator::BracketAdd => operands[idx] = operand1 + operand2,
+        Operator::Subtract | Operator::BracketSubtract => operands[idx] = operand1 - operand2,
+        Operator::Multiply | Operator::BracketMultiply => operands[idx] = operand1 * operand2,
+        Operator::Divide | Operator::BracketDivide => operands[idx] = operand1 / operand2,
+    }
+
+    operands.remove(idx + 1);
+    operators.remove(idx);
+}
+
+fn eval(expr: &String) -> f64 {
+    let mut in_bracket = false;
+    let mut operands = Vec::new();
+    let mut operators = Vec::new();
+
+    for c in expr.chars() {
+        if DIGITS.contains(&c) {
+            operands.push(c.to_digit(10).unwrap() as f64);
+        } else if SYMBOLS.contains(&c) {
+            operators.push(get_op(c, in_bracket));
+        } else if c == '(' {
+            in_bracket = true;
+        } else if c == ')' {
+            in_bracket = false;
+        }
+    }
+
+    while !operators.is_empty() {
+        let mut idx = operators.iter()
+            .position(|&o| o == Operator::BracketMultiply || o == Operator::BracketDivide);
+        if idx == None {
+            idx = operators.iter()
+                .position(|&o| o == Operator::BracketAdd || o == Operator::BracketSubtract);
+        }
+        if idx == None {
+            idx = operators.iter()
+                .position(|&o| o == Operator::Multiply || o == Operator::Divide);
+        }
+        if idx == None {
+            idx = operators.iter()
+                .position(|&o| o == Operator::Add || o == Operator::Subtract);
+        }
+
+        eval_op(&mut operands, &mut operators, idx.expect("Valid expression"));
+    }
+
+    operands[0]
+}
 
 #[derive(Clone)]
 struct Solution {
-    numbers: Vec<String>,
-    operators: Vec<String>,
+    numbers: Vec<char>,
+    operators: Vec<char>,
     brackets: (usize, usize),
 }
 
@@ -15,26 +90,21 @@ impl Solution {
         let mut expr = String::new();
         for i in 0..self.numbers.len() {
             if self.brackets.0 == i {
-                expr.push('[');
+                expr.push('(');
             }
-            expr.push_str(&self.numbers[i]);
+            expr.push(self.numbers[i]);
             if self.brackets.1 == i {
-                expr.push(']');
+                expr.push(')');
             }
             if i < self.operators.len() {
-                expr.push_str(&self.operators[i]);
+                expr.push(self.operators[i]);
             }
         }
         expr
     }
 
     fn calculate(&self) -> f64 {
-        let mut ns = EmptyNamespace;
-        if let Ok(result) = ez_eval(&self.to_expr(), &mut ns) {
-            result
-        } else {
-            panic!("Invalid expression: {}", self.to_expr());
-        }
+        eval(&self.to_expr())
     }
 }
 
@@ -48,10 +118,10 @@ fn for_each_num(
                 for l in k..DIGITS.len() {
                     let new_sol = Solution {
                         numbers: vec![
-                            DIGITS[i].to_string(),
-                            DIGITS[j].to_string(),
-                            DIGITS[k].to_string(),
-                            DIGITS[l].to_string(),
+                            DIGITS[i],
+                            DIGITS[j],
+                            DIGITS[k],
+                            DIGITS[l],
                         ],
                         ..base_sol.clone()
                     };
@@ -76,10 +146,10 @@ fn for_each_dist_num(
                 for l in (k + 1)..DIGITS.len() {
                     let new_sol = Solution {
                         numbers: vec![
-                            DIGITS[i].to_string(),
-                            DIGITS[j].to_string(),
-                            DIGITS[k].to_string(),
-                            DIGITS[l].to_string(),
+                            DIGITS[i],
+                            DIGITS[j],
+                            DIGITS[k],
+                            DIGITS[l],
                         ],
                         ..base_sol.clone()
                     };
@@ -132,7 +202,7 @@ fn for_each_op(base_sol: &Solution, callback: &mut impl FnMut(&Solution) -> bool
         for op2 in SYMBOLS {
             for op3 in SYMBOLS {
                 let new_sol = Solution {
-                    operators: vec![op1.to_string(), op2.to_string(), op3.to_string()],
+                    operators: vec![*op1, *op2, *op3],
                     ..base_sol.clone()
                 };
                 if callback(&new_sol) {
@@ -168,7 +238,7 @@ fn search(title: &str, num_iter: &dyn Fn(&Solution, &mut dyn FnMut(&Solution) ->
         brackets: (0, 0),
     };
 
-    let mut impossible_nums: Vec<Vec<String>> = Vec::new();
+    let mut impossible_nums: Vec<Vec<char>> = Vec::new();
 
     num_iter(&solution, &mut |sol1| {
         let possible = for_each_num_shuffle(sol1, &mut |sol2| {
